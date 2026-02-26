aefb703fc23d29579ea3ada6ae02ebb38c10e25c
Celine Ingabire <ingabireceline54@gmail.com>
2026-02-26 14:29:21 +0200

    chore: fix repository structure and add gitignore

---
+++ test.java
@@ -0,0 +1,14 @@
+
+
+public class test {
+
+    public static void validateSetup() {
+        System.out.println("Validating the setup of the project...");
+        
+    }
+
+    public static void main(String[] args) {
+        validateSetup();
+        System.out.println("Setup complete.");
+    }
+}
